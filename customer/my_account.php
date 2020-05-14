3ab149a648685b030ba491af536441a5f370081c
Black-Sinatra <afolabiolatoye@gmail.com>
2020-05-14 23:59:02 +0100

    Second Sprint Initial Commit

--- customer/my_account.php
+++ customer/my_account.php
@@ -18,7 +18,7 @@ include("functions/functions.php");
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>M-Dev Store</title>
+    <title>GROUP STORE</title>
     <link rel="stylesheet" href="styles/bootstrap-337.min.css">
     <link rel="stylesheet" href="font-awsome/css/font-awesome.min.css">
     <link rel="stylesheet" href="styles/style.css">
